9c75adf104ad696f09e48511a69dba3bcc32102d
Zhong <shazlin4663@yahoo.com>
2013-08-22 21:44:01 -0400

    updateLogin

--- src/com/app/closet/LoginActivity.java
+++ src/com/app/closet/LoginActivity.java
@@ -3,9 +3,7 @@ package com.app.closet;
 import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,18 +15,15 @@ import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
-	private Button btnLogin, btnSignup;
+	private Button btnLogin, btnSignup, btnResetPassword;
 	private EditText etUsername, etPassword;
 	public static final String APPLICATION_ID = "oN5bb0TVbdKMa7Hxws8kHUqw5T5muZltxnFNkaRK";
 	public static final String CLIENT_KEY = "519GBB9lefNbw70KZFDqFH8XR0sQAYMLEPRcQcwy";
-	private ParseUser parseUser;
-	public static ImageData imageData = new ImageData();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-//		ParseObject.registerSubclass(ImageData.class);
 		Parse.initialize(LoginActivity.this, APPLICATION_ID, CLIENT_KEY);
 
 		setContentView(R.layout.login_screen_layout);
@@ -37,59 +32,33 @@ public class LoginActivity extends Activity {
 		btnSignup = (Button) findViewById(R.id.btnSignup);
 		etUsername = (EditText) findViewById(R.id.etUsername);
 		etPassword = (EditText) findViewById(R.id.etPassword);
+		btnResetPassword = (Button) findViewById(R.id.btnResetPassword);
 
+		btnResetPassword.setOnClickListener(new ResetPasswordListener());
 		btnLogin.setOnClickListener(new LoginListener());
 		btnSignup.setOnClickListener(new SignupListener());
 	}
-	private void showToast (String message) {
-		Toast toast = Toast.makeText(LoginActivity.this,
-				message, Toast.LENGTH_LONG);
+
+	private void showToast(String message) {
+		Toast toast = Toast.makeText(LoginActivity.this, message,
+				Toast.LENGTH_LONG);
 		toast.show();
 	}
-	private void createParseObject () {
-		imageData.setParseCloth(new ParseObject("ClothImage"));
-		imageData.setParsePant(new ParseObject("PantImage"));
-		imageData.setParseShoe(new ParseObject("ShoeImage"));
-		imageData.setParseAccessory(new ParseObject("AccessoryImage"));
+
+	private final class ResetPasswordListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			startActivity(new Intent(LoginActivity.this, ResetPassword.class));
+		}
+
 	}
+
 	private final class SignupListener implements OnClickListener {
 
 		@Override
 		public void onClick(View v) {
-			String username = etUsername.getText().toString();
-			String password = etPassword.getText().toString();
-
-			if (username != null && password != null && !username.isEmpty()
-					&& !password.isEmpty()) {
-
-				ParseUser user = new ParseUser();
-				user.setUsername(username);
-				user.setPassword(password);
-				user.signUpInBackground(new SignUpCallback() {
-
-					@Override
-					public void done(ParseException e) {
-						if (e == null) {
-							showToast("You have sign up successful.");
-							startActivity(new Intent(LoginActivity.this, MainActivity.class));
-							
-							etUsername.setText("");
-							etPassword.setText("");
-							
-							
-//							createParseObject();
-//							
-							ParseObject closet = new ParseObject("Closet");
-							
-							parseUser = ParseUser.getCurrentUser();
-							parseUser.put("Closet", closet);			
-							parseUser.saveInBackground();
-						} else 
-							showToast("The username has been used.");
-					}
-				});
-			} else 
-				showToast("Please Enter username and password");
+			startActivity(new Intent(LoginActivity.this, Registration.class));
 		}
 	}
 
@@ -106,19 +75,27 @@ public class LoginActivity extends Activity {
 
 							@Override
 							public void done(ParseUser user, ParseException e) {
-								if (user != null) { 
-									showToast("SignIn successful");
+								if (user != null) {
+									showToast("SignIn successfully");
 									startActivity(new Intent(
 											LoginActivity.this,
 											MainActivity.class));
 									etUsername.setText("");
 									etPassword.setText("");
-								} else 
+								} else
 									showToast("Failed Login-Enter correct username and password");
 							}
 						});
-			} else 
-				showToast("Please Enter username and password");
+			} else {
+				if (username.isEmpty())
+					etUsername.setError("Enter username");
+				else if (password.isEmpty())
+					etPassword.setError("Enter password");
+				else {
+					etUsername.setError("Enter username");
+					etPassword.setError("Enter password");
+				}
+			}
 		}
 	}
 }
